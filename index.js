1eb9b11a55488a7e9077b1afc3936ea35c1d78fa
Sadruddin Mohammed <suavesalman@gmail.com>
2022-08-31 21:46:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Kubernetes!');
+  res.end('Hello World from OpenShift!');
 });
 server.listen(8080, function() {
 console.log('Server running on port 8080');
